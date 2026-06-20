412eb288610b2547b87547cf15e0aa457254390e
Phuziks <lvoropayev3@gmail.com>
2026-06-20 17:24:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, render_template_string
+import os
 
 app = Flask(__name__)
+
+STATUS_FILE = 'status.txt'
 
 HTML_TEMPLATE = '''
 <!DOCTYPE html>
@@ -16,10 +19,23 @@ HTML_TEMPLATE = '''
         <a href="/?status=on">Включить (ON)</a> |
         <a href="/?status=off">Выключить (OFF)</a>
     </p>
-
+    
+    <p>Текущий статус сохранен в файле: {{ status_file }}</p>
 </body>
 </html>
 '''
+
+
+def read_status():
+    if os.path.exists(STATUS_FILE):
+        with open(STATUS_FILE, 'r') as f:
+            return f.read().strip()
+    return 'OFF'
+
+
+def write_status(status):
+    with open(STATUS_FILE, 'w') as f:
+        f.write(status)
 
 
 @app.route('/')
@@ -28,12 +44,16 @@ def status():
 
     if status_param == 'on':
         status_text = 'ON'
+        write_status(status_text)
     elif status_param == 'off':
         status_text = 'OFF'
+        write_status(status_text)
     else:
-        status_text = 'OFF'
+        status_text = read_status()
 
-    return render_template_string(HTML_TEMPLATE, status_text=status_text)
+    return render_template_string(HTML_TEMPLATE, 
+                                 status_text=status_text,
+                                 status_file=STATUS_FILE)
 
 
 if __name__ == '__main__':
